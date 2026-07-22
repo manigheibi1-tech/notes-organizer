436610bfb446ba2e224ff63041522141c37ba188
Mani Gheibi <manigheibi1@gmail.com>
2026-07-23 01:24:18 +0330

    add course list view, template, and URL routing

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Course
 
-# Create your views here.
+@login_required
+def course_list(request):
+    courses = Course.objects.filter(owner=request.user)
+    return render(request, 'notes/course_list.html', {'courses': courses})
